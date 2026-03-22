de23aa3d6748c6b178ed879b6f6a7e7cb959a82b
corwinjstone <corwinjstone@gmail.com>
2026-03-22 11:24:38 -0500

    Delete capitalonetravel directory

--- capitalonetravel/src/components/layout/Header.jsx
+++
@@ -1,9 +0,0 @@
-import NavBar from "../navigation/NavBar";
-
-export default function Header() {
-  return (
-    <header className="sticky top-0 z-50 shadow-md">
-      <NavBar />
-    </header>
-  );
-}
--- capitalonetravel/src/hooks/useSearchTab.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export function useSearchTab(defaultTab = "flights") {
-  const [activeTab, setActiveTab] = useState(defaultTab);
-  const tabs = ["flights", "hotels", "packages", "cars"];
-  return { activeTab, setActiveTab, tabs };
-}
